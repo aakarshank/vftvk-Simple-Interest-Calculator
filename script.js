ca14f74616cb69e721908c8a3e29ee7b083d46cd
aakarshank <empirebird13@gmail.com>
2021-09-12 12:52:32 -0500

    Final touches

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     var years = document.getElementById('years').value;
     var interest = principal * years * rate/100;
     var year = new Date().getFullYear()+parseInt(years);
-    if (principal == '0'){
+    if (principal <= 0){
         alert('Enter a positive number');
         return;
     }
